5033ea18aad74a0045672bb156fcef5dd7503727
Morten Winther Olsson <18525061+olwimo@users.noreply.github.com>
2023-12-31 10:07:45 +0000

    werkz

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 . .env
-docker build . -t "olwimo/saleor-storefront:0.0.1" --build-arg NEXT_PUBLIC_SALEOR_API_URL="${NEXT_PUBLIC_SALEOR_API_URL}" --build-arg NEXT_PUBLIC_STOREFRONT_URL="${NEXT_PUBLIC_STOREFRONT_URL}"
+docker build . -t "olwimo/saleor-storefront:0.0.1" --build-arg NEXT_PUBLIC_SALEOR_API_URL="${NEXT_PUBLIC_SALEOR_API_URL}" --build-arg NEXT_PUBLIC_STOREFRONT_URL="${NEXT_PUBLIC_STOREFRONT_URL}" --build-arg SALEOR_APP_TOKEN="${SALEOR_APP_TOKEN}"
 docker tag "olwimo/saleor-storefront:0.0.1" "olwimo/saleor-storefront:latest"
 docker push -a "olwimo/saleor-storefront"
